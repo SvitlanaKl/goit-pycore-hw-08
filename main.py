72b0d77725fde2951466a516796b91d658431da5
svitlanakl <sv.klepka@gmail.com>
2024-05-19 15:45:32 +0300

    copied data from task

---
+++ main.py
@@ -0,0 +1,19 @@
+import pickle
+
+def save_data(book, filename="addressbook.pkl"):
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()  # Повернення нової адресної книги, якщо файл не знайдено
+    
+    def main():
+    book = load_data()
+
+    # Основний цикл програми
+
+    save_data(book)  # Викликати перед виходом з програми
